88d54b48836986a125b3def2ed1483cefb8a8e9e
Awalto <133243023+Awalto@users.noreply.github.com>
2026-02-13 11:07:05 -0500

    Add files via upload

---
+++ Task_2.s
@@ -0,0 +1,44 @@
+.section .bss
+.globl ram
+.lcomm ram, 256          # Reserve 256 bytes of RAM (uninitialized memory)
+
+.section .text
+.globl fill_ram_ind         # Make function visible to C program
+
+# Store FFh into RAM locations 50H - 58H using indirect addressing
+
+
+fill_ram_ind:
+    lea ram+0x50(%rip), %rdi 
+    movb $0xFF, (%rdi)
+    
+    lea ram+0x51(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x52(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x53(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x54(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x5(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x56(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x57(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+    lea ram+0x58(%rip), %rdi
+    movb $0xFF, (%rdi)
+
+
+    
+
+    ret                 # Return control back to C program
+
+.section .note.GNU-stack,"",@progbits
